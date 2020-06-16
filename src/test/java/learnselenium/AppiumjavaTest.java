595853129c480058d3d3aee72218c660049a655c
kasthuri <kasthuri.sunderlingam@gmail.com>
2020-06-16 19:18:52 +0100

    First changes

--- src/test/java/learnselenium/AppiumjavaTest.java
+++ src/test/java/learnselenium/AppiumjavaTest.java
@@ -9,11 +9,13 @@ public class AppiumjavaTest {
 	@Test
 	public void mobilelogin() {
 		System.out.println("This is mobile login");
+		System.out.println("Mobile login2");
 	}
 	
 	@Test
 	public void emulatortest() {
 		System.out.println("Mobile testcase execution");
+		System.out.println("This is repeat test execution of appium");
 	}
 	
 	
